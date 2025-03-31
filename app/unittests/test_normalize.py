d47a5696a3c1d4928c8bbeaa013b4171e1ecf0cf
Ike-li <viktorl@163.com>
2025-03-31 21:09:03 +0800

    .

--- app/unittests/test_normalize.py
+++ app/unittests/test_normalize.py
@@ -1,17 +1,17 @@
 class FeatureNormalizer:
     """对特征数据进行标准化处理
 
-        采用z-score标准化方法：(x - μ) / σ
-        其中 μ 是均值，σ 是标准差
+    采用z-score标准化方法：(x - μ) / σ
+    其中 μ 是均值，σ 是标准差
 
-        参数:
-            features: 需要标准化的特征数据矩阵
+    参数:
+        features: 需要标准化的特征数据矩阵
 
-        返回:
-            features_normalized: 标准化后的特征数据
-            features_mean: 特征的均值
-            features_deviation: 特征的标准差
-        """
+    返回:
+        features_normalized: 标准化后的特征数据
+        features_mean: 特征的均值
+        features_deviation: 特征的标准差
+    """
 
     def __init__(self):
         pass
@@ -28,7 +28,3 @@ def test_feature_normalize():
 def test_normalizer_normalize():
     feature_normalizer = FeatureNormalizer()
     feature_normalizer.normalize()
-
-
-
-
